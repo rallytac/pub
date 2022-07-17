77a6da8a6e84228b764057e5ccb8b9ea438ffcb3
Shaun Botha <shaun.botha@ucdynamics.com>
2022-07-17 08:30:46 -0700

    Added some testing logic for nsm

--- misc/nsm/nsm.sh
+++ misc/nsm/nsm.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+# To run in test mode for a random number of seconds with constant restart
+# set NSM_MAX_RUN_SECS to a positive number
+
 while [ true ]
 do
-    ./onIdle.sh ''
-    python3 nsm.py --max-run-secs $(( $RANDOM % 30 + 30 )) --log-level 4 ${1} ${2} ${3} ${4} ${5} ${6}
-    ./onIdle.sh ''
+    if [[ "${NSM_MAX_RUN_SECS}" == "" ]]; then
+        ./onIdle.sh ''
+        python3 nsm.py --log-level 4 ${1} ${2} ${3} ${4} ${5} ${6}
+        ./onIdle.sh ''
+        break
+    else
+        ./onIdle.sh ''
+        python3 nsm.py --max-run-secs $(( $RANDOM % ${NSM_MAX_RUN_SECS} + ${NSM_MAX_RUN_SECS} )) --log-level 4 ${1} ${2} ${3} ${4} ${5} ${6}
+        ./onIdle.sh ''
+    fi
 done
