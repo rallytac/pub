acd602afb0ada7c9c184e08d2f330cbcccd9d2e0
Shaun Botha <shaun.botha@ucdynamics.com>
2025-09-10 18:37:30 -0700

    More fixes and cleanup

--- misc/nsm/test/nsm_old/onActive.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-GREEN='\033[1;32m'
-NC='\033[0m'
-printf "${GREEN}onActive, resource='${1}\n${NC}"
--- misc/nsm/test/nsm_old/onIdle.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-RED='\033[1;31m'
-NC='\033[0m'
-printf "${RED}onIdle, resource='${1}\n${NC}"
